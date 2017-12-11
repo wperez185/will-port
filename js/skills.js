7129b4c821011909e714d4783f5a1ec753a00fd4
Will <will@Ericks-MBP.fios-router.home>
2017-12-11 18:56:43 -0500

    first commit

---
+++ js/skills.js
@@ -0,0 +1,23 @@
+setTimeout(function() {
+  $('#html-percent').html('100%');
+},2800);
+
+setTimeout(function() {
+  $('#css-percent').html('90%');
+},3500);
+
+setTimeout(function() {
+  $('#javascript-percent').html('75%');
+},4200);
+
+setTimeout(function() {
+  $('#react-percent').html('50%');
+},4500);
+
+setTimeout(function() {
+  $('#node-percent').html('50%');
+},4900);
+
+setTimeout(function() {
+  $('#ruby-percent').html('40%');
+},5200);
